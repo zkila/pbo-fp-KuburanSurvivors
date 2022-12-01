f555493ec172124a4cb64b7588b3c53ee331d20e
unknown <jawahirulwildan@gmail.com>
2022-12-01 12:00:44 +0700

    gaero nambah opo

--- src/entity/Entity.java
+++ src/entity/Entity.java
@@ -1,5 +1,6 @@
 package entity;
 
+import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
 
 public class Entity {
@@ -8,5 +9,11 @@ public class Entity {
 	
 	public BufferedImage left, right ;
 	public String direction;
+	
+	public int spriteCounter = 0;
+	public int spriteNum = 1;
+	public Rectangle solidArea;
+	public boolean collisionOn = false;
+	
 	
 }
